b0aca477ff0ced27dcb01d61043e7af12bcbe3e5
graceabiolu838 <grace.olusegun@laetottenham.ac.uk>
2024-12-08 08:56:09 +0000

    finished ish

--- club_manage.py
+++ club_manage.py
@@ -14,18 +14,20 @@ class Person:
 
 class Member(Person):
     def __init__(self, name, age, contact, mem_id, sport, pfm_score):
-        super.__init__(name, age, contact)
-        self.mem_id = "M8962"
-        self.sport = "Track"
-        self.pfm_score = [10.54, 9.58, 10.49, 10.65, 19.31, 19.49, 50.37]
+        super().__init__(name, age, contact)
+        self.mem_id = mem_id 
+        self.sport = sport 
+        self.pfm_score = pfm_score 
     def set_mem_details(self, mem_id, sport):
         self.mem_id = mem_id
         self.sport = sport 
     def add_pfm(self, score):
         self.pfm_score.append(score)
     def calc_av(self, pfm_score):
-        av = sum(pfm_score)/len(pfm_score)
-        return 0 if not pfm_score else av
+        try:
+            av = sum(pfm_score)/len(pfm_score)
+            return av
+        except ZeroDivisionError: return 0
     def mem_summ(self):
         return f"Name: {self.name}\n\
             Age: {self.age}\n\
@@ -37,11 +39,11 @@ class Member(Person):
     
 class Coach(Person):
     def __init__(self, name, age, contact, coach_id, spec, pay, mentees):
-        super.__init__(name, age, contact)
-        self.coach_id = "C7014"
-        self.spec = "Track"
-        self.pay = 120000
-        self.mentees = [] #["Shacarri", "Sherika", "Syndey", "Julian", "Noah", "Zharnel", "Erriyon", "Dina", "Keely"]
+        super().__init__(name, age, contact)
+        self.coach_id = coach_id
+        self.spec = spec
+        self.pay = pay
+        self.mentees = mentees #["Shacarri", "Sherika", "Syndey", "Julian", "Noah", "Zharnel", "Erriyon", "Dina", "Keely"]
     def co_details(self, coach_id, spec, pay):
         self.coach_id = coach_id
         self.spec = spec
@@ -61,18 +63,25 @@ class Coach(Person):
             else: continue 
 
         return f"{x}"
-    
     def inc_pay(self, percent):
         #int(input("Number between 1 and 10- be wise\n"))
         self.pay*=(1+(percent*len(self.mentees))/100)
         return self.pay
+    def coach_sum(self):
+        return f"Name: {self.name}\n\
+            Age: {self.age}\n\
+            Contact: {self.contact}\n\
+            Coach ID: {self.coach_id}\n\
+            Speciality: {self.spec}\n\
+            Salary: {self.pay}\n\
+            Mentees: {self.mentees}\n"
 
 class Staff(Person):
     def __init_(self, name, age, contact, staff_id, pos, yrs):
-        super.__init__(name, age, contact)
-        self.staff_id = "S4836"
-        self.pos = "Club Secretary"
-        self.yrs = 132
+        super().__init__(name, age, contact)
+        self.staff_id = staff_id
+        self.pos = pos 
+        self.yrs = yrs
     def set_det(self, staff_id, pos, yrs):
         self.staff_id = staff_id
         self.pos = pos
@@ -88,9 +97,22 @@ class Staff(Person):
             Staff ID: {self.staff_id}\n\
             Position: {self.pos}\n\
             Years of Service: {self.yrs}\n"
-    
-grace = Member("Grace", 17, 7266727190)
-flo = Coach()
-worker = Staff()
+  
+grace = Member("Grace", 17, 37826496291, "M8962", "Track", [10.54, 9.58, 10.49, 10.65, 19.31, 19.49, 50.37])
+flo = Coach("Grace", 17, 37826496291, "C7014", "Track", 120000, [])
+worker = Staff("Grace", 17, 37826496291, "S4836", "Club Secretary", 132)
+
+flo.assign(grace)
+flo.inc_pay(15)
+grace.add_pfm(10)
+grace.calc_av(grace.pfm_score)
+worker.assist_mem(grace)
+worker.inc_yrs()
+
+grace.mem_summ()
+flo.coach_sum()
+worker.staff_summ()
+
+
 
 
